6e69890445a4279aa5b9ef557ecb2acd9b8f39d7
Allandantas21 <Allandantas21.ad@gmail.com>
2024-06-04 03:48:46 -0300

    add mensage on tester

--- test.sh
+++ test.sh
@@ -12,40 +12,43 @@ NB_OF_TESTS=1
 RESULTS_FOLDER='results'
 
 ## TEST FUNCTION DEFINITION
+# Função de teste
 run_test_case() {
-	CASE_NO=$1
-	CASE=$2
-	EXPECTED_OUTCOME=$3
-	FONT_COLOUR_BG=''
-	FONT_COLOUR=''
-	COLOR_RESET="\033[0m"
-	COLOR_YELLOW="\033[33m"
-	BLINK="\033[5m"
+    CASE_NO=$1
+    CASE=$2
+    EXPECTED_OUTCOME=$3
+    FONT_COLOUR_BG=''
 
-	i=1
-	time=0.1
+    i=1
+    time=0.1
 
-	if [[ $EXPECTED_OUTCOME == "should die" ]]
-	then
-		COLOUR_BG="\e[1;31mTest:"
-		FONT_COLOUR_BG="\e[41;30m"
-	else
-		COLOUR_BG="\e[1;32mTest:"
-		FONT_COLOUR_BG="\e[42;30m"
-	fi
+    if [[ $EXPECTED_OUTCOME == "should die" ]]; then
+        COLOUR_BG="\e[1;31mTest:"
+        FONT_COLOUR_BG="\e[41;30m"
+    else
+        COLOUR_BG="\e[1;32mTest:"
+        FONT_COLOUR_BG="\e[42;30m"
+    fi
 
-	mkdir -p "$RESULTS_FOLDER/$CASE_NO"
-	echo -e "$FONT_COLOUR_BG $CASE_NO: $CASE $EXPECTED_OUTCOME \e[0m"
-	while [ $i -le $NB_OF_TESTS ]
-	do
-		echo -e "$COLOUR_BG $i\e[0m"
-		echo -e "${BLINK} ${COLOR_YELLOW} Testing ${COLOR_RESET}"
-		$BIN_PATH $CASE > "$RESULTS_FOLDER/$CASE_NO/test$i"
-		cat "$RESULTS_FOLDER/$CASE_NO/test$i" | grep 'die'
-		sleep $time
-		i=$(( $i + 1 ))
-	done
-	echo
+    mkdir -p "$RESULTS_FOLDER/$CASE_NO"
+    echo -e "$FONT_COLOUR_BG $CASE_NO: $CASE $EXPECTED_OUTCOME \e[0m"
+    while [ $i -le $NB_OF_TESTS ]; do
+        echo -e "$COLOUR_BG $i\e[0m"
+        echo -e "${BLINK}${YELLOW}Testing${RESET}"
+        $BIN_PATH $CASE > "$RESULTS_FOLDER/$CASE_NO/test$i"
+
+        die_count=$(grep -c 'die' "$RESULTS_FOLDER/$CASE_NO/test$i")
+
+        if [ "$die_count" -eq 1 ]; then
+            echo -e "${GREEN}Success: Exactly one philosopher died as expected.${RESET}"
+        else
+            echo -e "${RED}Failure: Unexpected number of philosophers died (${die_count}).${RESET}"
+        fi
+
+        sleep $time
+        i=$((i + 1))
+    done
+    echo
 }
 
 ## RUN SCRIPT
